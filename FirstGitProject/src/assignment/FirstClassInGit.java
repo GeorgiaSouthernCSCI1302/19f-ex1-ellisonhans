24598da87e4eb05c4a8a248ed7f5cfe937e9a69d
eh04067 <eh04067@CEIT2553214X017.ad.georgiasouthern.edu>
2019-08-21 16:50:48 -0400

    stored user input of last name and displayed in console

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,19 @@
+
+
 package assignment;
+
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
+	
 	public static void main(String[] args) {
-		System.out.println(" Hello my name is Ellison! ");
+		System.out.println(" What is your last name? ");
+		Scanner sc = new Scanner (System.in);
+		String lastname = sc.nextLine();
+		System.out.println("The user's last name is " + lastname + "!");
+		
+		
 		
 		//add a statement to display your name in the console
 		
